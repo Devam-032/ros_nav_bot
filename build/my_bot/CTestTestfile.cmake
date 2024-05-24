72e16ab46aa86a1171c3a837d6646e0968ddc3ff
Devam-032 <devamshah82@gmail.com>
2024-05-24 20:00:57 +0530

    first commit

---
+++ build/my_bot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rover/devam_6otu/src/my_bot
+# Build directory: /home/rover/devam_6otu/build/my_bot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
