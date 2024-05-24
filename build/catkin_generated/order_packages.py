e6f6bc4f7e91c6438493704ec3df9362c5d0d902
Devam-032 <devamshah82@gmail.com>
2024-05-24 20:22:43 +0530

    second_commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/rover/devam_6otu/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/rover/ydlidar_ws/devel;/opt/ros/noetic'.split(';') if '/home/rover/ydlidar_ws/devel;/opt/ros/noetic' != '' else []
